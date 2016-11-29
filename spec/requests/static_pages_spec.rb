adffff33a0d06b744326f7b1d8c1f2000e1b8ea4
ghiropon <ghiropon@gmail.com>
2016-11-30 01:10:15 +0900

    Finish user signup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,7 +15,7 @@ describe "Static pages" do
     before { visit help_path }
 
     it { should have_content('Help') }
-    it { should  have_title(full_title('Help')) }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
